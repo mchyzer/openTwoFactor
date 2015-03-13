32996ac2ae328ad1617829a1546806a50a1ca8b4
mchyzer <mchyzer@isc.upenn.edu>
2015-03-13 02:08:15 -0400

    add source additions

--- twoFactor/database/oracle/views/TF_PERSON_SOURCE_ADMIN_V.sql
+++ twoFactor/database/oracle/views/TF_PERSON_SOURCE_ADMIN_V.sql
@@ -1,4 +1,4 @@
-/* Formatted on 3/12/2015 8:39:16 PM (QP5 v5.252.13127.32847) */
+/* Formatted on 3/13/2015 2:01:52 AM (QP5 v5.252.13127.32847) */
 CREATE OR REPLACE FORCE VIEW TF_PERSON_SOURCE_ADMIN_V
 (
    PENN_ID,
@@ -20,12 +20,15 @@ AS
              WHEN CPV.IS_ACTIVE_FACULTY = 'Y' THEN 'T'
              WHEN CPV.IS_ACTIVE_STAFF = 'Y' THEN 'T'
              WHEN CPV.IS_ACTIVE_STUDENT = 'Y' THEN 'T'
+             WHEN TSMA.ACTIVE = 'T' THEN 'T'
              ELSE 'F'
           END
              AS active
      /* (select 'T' from authzadm.PROJECT_TF_ACTIVE_USERS_V ptauv where ptauv.penn_id = cpv.penn_id) as active */
-     FROM pcdadmin.computed_person_v cpv
-    WHERE kerberos_principal IS NOT NULL;
+     FROM pcdadmin.computed_person_v cpv, tf_source_make_active tsma
+    WHERE kerberos_principal IS NOT NULL
+      AND TSMA.PENN_ID (+) = CPV.CHAR_PENN_ID ;
+    
 
 COMMENT ON TABLE TF_PERSON_SOURCE_ADMIN_V IS 'person source to lookup and search for subjects in the two factor project for admin users';
 
