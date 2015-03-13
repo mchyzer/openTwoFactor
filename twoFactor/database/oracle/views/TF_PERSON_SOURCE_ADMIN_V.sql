cc5e006c441b7708d98ed5405fab61b8ba066799
mchyzer <mchyzer@isc.upenn.edu>
2015-03-12 20:49:20 -0400

    from database

--- twoFactor/database/oracle/views/TF_PERSON_SOURCE_ADMIN_V.sql
+++ twoFactor/database/oracle/views/TF_PERSON_SOURCE_ADMIN_V.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FORCE VIEW tf_person_source_admin_v
+/* Formatted on 3/12/2015 8:39:16 PM (QP5 v5.252.13127.32847) */
+CREATE OR REPLACE FORCE VIEW TF_PERSON_SOURCE_ADMIN_V
 (
    PENN_ID,
    PENNNAME,
@@ -25,18 +26,23 @@ AS
      /* (select 'T' from authzadm.PROJECT_TF_ACTIVE_USERS_V ptauv where ptauv.penn_id = cpv.penn_id) as active */
      FROM pcdadmin.computed_person_v cpv
     WHERE kerberos_principal IS NOT NULL;
-COMMENT ON TABLE tf_person_source_admin_v IS 'person source to lookup and search for subjects in the two factor project';
 
-COMMENT ON COLUMN tf_person_source_admin_v.PENN_ID IS 'numeric penn_id';
+COMMENT ON TABLE TF_PERSON_SOURCE_ADMIN_V IS 'person source to lookup and search for subjects in the two factor project for admin users';
 
-COMMENT ON COLUMN tf_person_source_admin_v.PENNNAME IS 'pennkey of the user';
+COMMENT ON COLUMN TF_PERSON_SOURCE_ADMIN_V.PENN_ID IS 'numeric penn_id';
 
-COMMENT ON COLUMN tf_person_source_admin_v.DESCRIPTION IS 'description to show on screen';
+COMMENT ON COLUMN TF_PERSON_SOURCE_ADMIN_V.PENNNAME IS 'pennkey of the user';
 
-COMMENT ON COLUMN tf_person_source_admin_v.SEARCH_DESCRIPTION IS 'lower search description to search with';
+COMMENT ON COLUMN TF_PERSON_SOURCE_ADMIN_V.DESCRIPTION IS 'description to show on screen';
 
-COMMENT ON COLUMN tf_person_source_admin_v.NAME IS 'name of user';
+COMMENT ON COLUMN TF_PERSON_SOURCE_ADMIN_V.SEARCH_DESCRIPTION IS 'lower search description to search with';
 
-COMMENT ON COLUMN tf_person_source_admin_v.EMAIL IS 'email of user';
+COMMENT ON COLUMN TF_PERSON_SOURCE_ADMIN_V.NAME IS 'name of user';
 
-COMMENT ON COLUMN tf_person_source_admin_v.ACTIVE IS 'T if active, F if not';
+COMMENT ON COLUMN TF_PERSON_SOURCE_ADMIN_V.EMAIL IS 'email of user';
+
+COMMENT ON COLUMN TF_PERSON_SOURCE_ADMIN_V.ACTIVE IS 'T if active, F if not';
+
+
+
+GRANT SELECT ON TF_PERSON_SOURCE_ADMIN_V TO TWO_FACTOR_ADMIN;
