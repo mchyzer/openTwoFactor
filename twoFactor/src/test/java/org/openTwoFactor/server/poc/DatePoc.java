eac2fdce7547ac3f88b73e0a93fd05fe79c4cc96
mchyzer <mchyzer@isc.upenn.edu>
2015-06-08 11:09:27 -0400

    tweak text

---
+++ twoFactor/src/test/java/org/openTwoFactor/server/poc/DatePoc.java
@@ -0,0 +1,30 @@
+/**
+ * @author mchyzer
+ * $Id$
+ */
+package org.openTwoFactor.server.poc;
+
+import java.util.Date;
+
+
+/**
+ *
+ */
+public class DatePoc {
+
+  /**
+   * 
+   */
+  public DatePoc() {
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    Date date = new Date(1433524554723L);
+    System.out.println(date);
+    
+  }
+
+}
