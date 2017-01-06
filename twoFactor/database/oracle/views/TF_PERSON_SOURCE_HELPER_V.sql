ba1c6a8a2300e65fb872b9cbc9707fdccc023b50
mchyzer <mchyzer@isc.upenn.edu>
2017-01-06 12:25:37 -0500

    TWOFACT-174: prompt for birth date when opting in

--- twoFactor/database/oracle/views/TF_PERSON_SOURCE_HELPER_V.sql
+++ twoFactor/database/oracle/views/TF_PERSON_SOURCE_HELPER_V.sql
@@ -1,4 +1,4 @@
-/* Formatted on 3/13/2015 2:01:52 AM (QP5 v5.252.13127.32847) */
+/* Formatted on 1/6/2017 12:24:37 PM (QP5 v5.252.13127.32847) */
 CREATE OR REPLACE FORCE VIEW TF_PERSON_SOURCE_HELPER_V
 (
    PENN_ID,
@@ -10,11 +10,13 @@ CREATE OR REPLACE FORCE VIEW TF_PERSON_SOURCE_HELPER_V
    SEARCH_DESCRIPTION_PENN,
    NAME_PENN,
    EMAIL,
-   ACTIVE
+   ACTIVE,
+   BIRTH_DATE
 )
+   BEQUEATH DEFINER
 AS
    SELECT char_penn_id AS penn_id,
-          kerberos_principal AS pennname,          
+          kerberos_principal AS pennname,
           description,
           search_description,
           directory_pref_cent_disp_name AS name,
@@ -27,16 +29,16 @@ AS
              WHEN CPV.IS_ACTIVE_STAFF = 'Y' THEN 'T'
              WHEN CPV.IS_ACTIVE_STUDENT = 'Y' THEN 'T'
              WHEN CPV.IS_ACTIVE_UPHS = 'Y' THEN 'T'
-             WHEN CPV.ALL_ACTIVE_AFFILIATIONS like '%SERV%' THEN 'T'
+             WHEN CPV.ALL_ACTIVE_AFFILIATIONS LIKE '%SERV%' THEN 'T'
              WHEN TSMA.ACTIVE = 'T' THEN 'T'
              ELSE 'F'
           END
-             AS active
+             AS active,
+          cpv.BIRTH_DATE
      /* (select 'T' from authzadm.PROJECT_TF_ACTIVE_USERS_V ptauv where ptauv.penn_id = cpv.penn_id) as active */
      FROM pcdadmin.computed_person_v cpv, tf_source_make_active tsma
-    WHERE kerberos_principal IS NOT NULL
-      AND TSMA.PENN_ID (+) = CPV.CHAR_PENN_ID ;
-    
+    WHERE     kerberos_principal IS NOT NULL
+          AND TSMA.PENN_ID(+) = CPV.CHAR_PENN_ID;
 
 COMMENT ON TABLE TF_PERSON_SOURCE_HELPER_V IS 'person source to lookup and search for subjects in the two factor project for admin users';
 
@@ -60,5 +62,4 @@ COMMENT ON COLUMN TF_PERSON_SOURCE_HELPER_V.EMAIL IS 'email of user';
 
 COMMENT ON COLUMN TF_PERSON_SOURCE_HELPER_V.ACTIVE IS 'T if active, F if not';
 
-
-
+COMMENT ON COLUMN TF_PERSON_SOURCE_HELPER_V.BIRTH_DATE IS 'birth date if we have it';
