73daa0fe287923d9083a167d1ac5de3442cd4eb7
mchyzer <mchyzer@isc.upenn.edu>
2017-07-03 23:11:30 -0400

    last four

--- twoFactor/database/oracle/views/TF_PERSON_SOURCE_ADMIN_ALL_V.sql
+++ twoFactor/database/oracle/views/TF_PERSON_SOURCE_ADMIN_ALL_V.sql
@@ -1,4 +1,4 @@
-/* Formatted on 1/6/2017 2:04:20 PM (QP5 v5.252.13127.32847) */
+/* Formatted on 7/3/2017 11:10:01 PM (QP5 v5.252.13127.32847) */
 CREATE OR REPLACE FORCE VIEW TF_PERSON_SOURCE_ADMIN_ALL_V
 (
    PENN_ID,
@@ -8,7 +8,8 @@ CREATE OR REPLACE FORCE VIEW TF_PERSON_SOURCE_ADMIN_ALL_V
    NAME,
    EMAIL,
    ACTIVE,
-   BIRTH_DATE
+   BIRTH_DATE,
+   last_four
 )
    BEQUEATH DEFINER
 AS
@@ -19,10 +20,10 @@ AS
           name,
           email,
           'T' AS active,
-          birth_date
+          birth_date,
+          last_four
      FROM tf_person_source_admin_v;
 
 COMMENT ON TABLE TF_PERSON_SOURCE_ADMIN_ALL_V IS 'all view for testing where everyone is active';
-
 
 GRANT SELECT ON TF_PERSON_SOURCE_ADMIN_ALL_V TO TWO_FACTOR_ADMIN WITH GRANT OPTION;
